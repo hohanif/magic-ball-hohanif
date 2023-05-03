596a1e1916ae02305048a0e06993bdf44d76b8d1
hohanif <homayoon.moha@gmail.com>
2023-05-02 21:33:06 -0700

    ball page created

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,24 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: null,
+      title: "Magic Ball",
+      home: BallPage(),
     ),
   );
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ask Me Anything"),
+        backgroundColor: Colors.blue[900],
+      ),
+      backgroundColor: Colors.blue,
+      body: Container(),
+    );
+  }
 }
